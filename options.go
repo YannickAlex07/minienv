c0b185fdf8fcebffb32a76d0d684f0d6b672ed01
Yannick Alexander <yannick@alexanderdev.io>
2024-05-02 21:11:15 +0200

    added file reading

--- options.go
+++ options.go
@@ -1,6 +1,10 @@
 package minienv
 
-import "os"
+import (
+	"bufio"
+	"os"
+	"regexp"
+)
 
 func WithOverrides(overrides map[string]string) Option {
 	return func(m map[string]string) error {
@@ -38,11 +42,41 @@ func WithFile(canFail bool, files ...string) Option {
 }
 
 func parseEnvFile(path string) (map[string]string, error) {
+	// open file
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	return nil, nil
+	overrides := map[string]string{}
+
+	// scan file
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	// compile regex
+	r, err := regexp.Compile(`^(?P<key>\w+)=["']?(?P<value>[^'"]*)['"]?.*$`)
+	if err != nil {
+		return nil, err
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// skip empty lines
+		if len(line) == 0 {
+			continue
+		}
+
+		// check if line is a valid env line
+		matches := r.FindStringSubmatch(line)
+		if len(matches) == 0 || matches == nil {
+			continue
+		}
+
+		overrides[matches[r.SubexpIndex("key")]] = matches[r.SubexpIndex("value")]
+	}
+
+	return overrides, nil
 }
